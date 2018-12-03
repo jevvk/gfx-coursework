eff8635d8e84a6531a3e07cad5ed6fbe51c030ad
LiNoimi Semain <iplay.addicted@gmail.com>
2018-12-03 21:29:58 +0000

    Add width and height to python script

--- scripts/bmp2bytearray.py
+++ scripts/bmp2bytearray.py
@@ -6,11 +6,19 @@ name = sys.argv[1]
 outfile = sys.argv[3]
 im = Image.open(sys.argv[2])
 
-p = np.array(im).reshape(-1, 4)[:, [0, 1, 2]].flatten()
+p = np.array(im)
+
+height = p.shape[1]
+width = p.shape[1]
+
+p = p.reshape(-1, 4)[:, [0, 1, 2]].flatten()
 p = bytearray(p)
 
 array_string = ','.join('0x%02x'%i for i in p)
-result = "const char %s[] = { %s };" % (name, array_string)
+result = """const char {name}[] = {{ {0} }};
+const int {name}_width = {1};
+const int {name}_height = {2};
+""".format(array_string, height, width, name=name)
 
 f = open(sys.argv[3], 'w')
 f.write(result)
