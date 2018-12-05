068c103a3be25afc7332d09c1b0ab4afb2347475
LiNoimi Semain <iplay.addicted@gmail.com>
2018-12-05 15:54:08 +0000

    Add cl kernels to filelist

--- src/Files.h
+++ src/Files.h
@@ -2,7 +2,30 @@
 
 extern const char _file_simulation_frag[];
 extern const int _file_simulation_frag_length;
+
 extern const char _file_simulation_vert[];
 extern const int _file_simulation_vert_length;
+
 extern const char _file_body_bmp[];
 extern const int _file_body_bmp_height, _file_body_bmp_width;
+
+extern const char _file_boundingbox_cl[];
+extern const int _file_boundingbox_cl_length;
+
+extern const char _file_buildtree_cl[];
+extern const int _file_buildtree_cl_length;
+
+extern const char _file_calculateforce_cl[];
+extern const int _file_calculateforce_cl_length;
+
+extern const char _file_copyvertices_cl[];
+extern const int _file_copyvertices_cl_length;
+
+extern const char _file_integrate_cl[];
+extern const int _file_integrate_cl_length;
+
+extern const char _file_sort_cl[];
+extern const int _file_sort_cl_length;
+
+extern const char _file_summarizetree_cl[];
+extern const int _file_summarizetree_cl_length;
